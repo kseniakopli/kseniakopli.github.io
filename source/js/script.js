6f2c587c049529a3d5f95e9a9a44884290e19304
kseniakopli <55592072+kseniakopli@users.noreply.github.com>
2020-07-14 15:18:14 +0300

    added menu to mobule and tablet versions

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,27 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// Получение данных
+const menu = document.querySelector(".menu");
+const menuButton = menu.querySelector(".menu__btn");
+const menuLink = menu.querySelectorAll(".menu__link");
 
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+//События
+menuButton.addEventListener("click", function () {
+  applyChanges();
 });
+
+for (let i = 0; i < menuLink.length; i++) {
+  menuLink[i].addEventListener("click", function () {
+    applyChanges();
+  });
+}
+
+//Функции
+
+function applyChanges() {
+  if (menu.classList.contains("menu--opened")) {
+    menu.classList.add("menu--closed");
+    menu.classList.remove("menu--opened");
+  } else {
+    menu.classList.add("menu--opened");
+    menu.classList.remove("menu--closed");
+  }
+}
