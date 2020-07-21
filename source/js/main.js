80de5af2cc9991cc540e11777b142a95e1f260ca
kseniakopli <55592072+kseniakopli@users.noreply.github.com>
2020-07-21 21:58:01 +0300

    fixed some bugs

--- source/js/main.js
+++ source/js/main.js
@@ -1,34 +1,36 @@
-'use strict';
+"use strict";
 // Получение данных
-var menuList = document.querySelector('.menu');
-var menuButton = menuList.querySelector('.menu__btn');
-var menuLink = menuList.querySelectorAll('.menu__link');
-var input = document.getElementById('phone');
+var menuList = document.querySelector(".menu");
+var menuButton = menuList.querySelector(".menu__btn");
+var menuLink = menuList.querySelectorAll(".menu__link");
+var input = document.getElementById("phone");
 
-menuList.classList.remove('menu--nojs');
+menuList.classList.remove("menu--nojs");
 
 // События
-menuButton.addEventListener('click', function () {
+menuButton.addEventListener("click", function () {
   applyChanges();
 });
 
 for (var i = 0; i < menuLink.length; i++) {
-  menuLink[i].addEventListener('click', function () {
+  menuLink[i].addEventListener("click", function () {
     applyChanges();
   });
 }
 
 input.oninvalid = function (event) {
-  event.target.setCustomValidity('Ошибка: в номере телефона нельзя указывать буквы');
+  event.target.setCustomValidity(
+    "Введите телефон в формате +7(XXX)XXXXXXX без использования букв"
+  );
 };
 
 // Функции
 function applyChanges() {
-  if (menuList.classList.contains('menu--opened')) {
-    menuList.classList.add('menu--closed');
-    menuList.classList.remove('menu--opened');
+  if (menuList.classList.contains("menu--opened")) {
+    menuList.classList.add("menu--closed");
+    menuList.classList.remove("menu--opened");
   } else {
-    menuList.classList.add('menu--opened');
-    menuList.classList.remove('menu--closed');
+    menuList.classList.add("menu--opened");
+    menuList.classList.remove("menu--closed");
   }
 }
